a9e188cb7d074ad3e35d374a5630460a2a8a9c1c
Michael Kreil <github@michael-kreil.de>
2023-02-24 13:04:10 +0100

    complete rewrite

--- bin/convert_fonts.js
+++ bin/convert_fonts.js
@@ -5,141 +5,114 @@ const fs = require('fs');
 const path = require('path');
 const fontnik = require('fontnik');
 const glyphCompose = require('@mapbox/glyph-pbf-composite');
-
-const DEBUG = false;
+const { resolve } = require('path');
+require('work-faster');
 
 const inputDir = path.resolve(__dirname, '../font-sources');
 const outputDir = path.resolve(__dirname, '../dist/fonts/');
 
-if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, {recursive:true});
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
 
 start()
 async function start() {
-	let fontnames = [];
-	const todos = getTodos();
+	const fonts = getFonts();
 
-	// would be much faster in parallel, but this is better for logging
-	let sizeSumTotal = 0;
-	for (let { dir, fonts } of todos) {
-		console.log('Directory [%s]:', path.relative(inputDir, dir));
-		sizeSumTotal += await processFonts(dir, fonts);
-	}
-	console.log('Total size %s KB', Math.round(sizeSumTotal/1024));
+	let sizePos = 0;
+	let sizeSum = fonts.reduce((s, f) => s + f.sizeIn, 0);
 
-	fs.writeFileSync(path.resolve(outputDir,"fonts.json"), JSON.stringify(fontnames,null,"\t"));
+	await fonts.forEachAsync(async font => {
+		let progress = (100 * sizePos / sizeSum).toFixed(1) + '%';
+		progress = ' '.repeat(8 - progress.length) + progress;
+		process.stdout.write('\u001b[2K\r' + progress + ' - ' + font.name)
 
-	function getTodos() {
-		const todos = [];
+		await makeGlyphs(font);
+		sizePos += font.sizeIn;
+	})
+	process.stdout.write('\u001b[2K\rFinished\n')
 
-		fs.readdirSync(inputDir).forEach(dir => {
-			if (dir.startsWith('_')) return;
-
-			dir = path.resolve(inputDir, dir);
-			if (fs.lstatSync(dir).isDirectory()) {
-				let fonts = false;
-				try {
-					fonts = require(path.resolve(dir, 'fonts.json'));
-					fonts.forEach(font => {
-						// skip sources starting with '//' -- these are "commented"
-						font.sources = font.sources.filter(f => !f.includes('//'));
-						fontnames.push(font.name);
-					});
-				} catch (e) {
-					if (e.code !== 'MODULE_NOT_FOUND') console.error(e);
-
-					fonts = [];
-					fs.readdirSync(dir).forEach(file => {
-						if (path.extname(file) === '.ttf' || path.extname(file) === '.otf') {
-							// compatible font name generation with genfontgl
-							let name = path.basename(file);
-							name = name.replace(/\..*?$/, '');
-							name = name.replace(/\-/g, '');
-							name = name.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
-							name = name.replace(/([A-Z])([A-Z][a-z])/g, '$1 $2');
-							name = name.replace(/\s+/, ' ').trim();
-							name = name.toLowerCase().replace(/\s/g, '_');
-							fonts.push({ name, sources: [path.basename(file)] });
-							fontnames.push(name);
-						}
-					});
-				}
-				if (fonts && (fonts.length > 0)) todos.push({ dir, fonts });
-			}
-		});
-
-		return todos;
-	}
+	let fontnames = fonts.map(f => f.name);
+	fs.writeFileSync(path.resolve(outputDir, 'fonts.json'), JSON.stringify(fontnames, null, '\t'));
 }
 
+function getFonts() {
+	const todos = [];
 
-async function processFonts(dir, fonts) {
+	fs.readdirSync(inputDir).forEach(dir => {
+		if (dir.startsWith('_')) return;
+
+		dir = path.resolve(inputDir, dir);
+		if (fs.lstatSync(dir).isDirectory()) {
+			let fonts = [];
+			try {
+				fonts = require(path.resolve(dir, 'fonts.json'));
+			} catch (e) {
+				if (e.code !== 'MODULE_NOT_FOUND') console.error(e);
+
+				fs.readdirSync(dir).forEach(file => {
+					if (path.extname(file) === '.ttf' || path.extname(file) === '.otf') {
+						// compatible font name generation with genfontgl
+						let name = path.basename(file);
+						name = name.replace(/\..*?$/, '');
+						name = name.replace(/\-/g, '');
+						name = name.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
+						name = name.replace(/([A-Z])([A-Z][a-z])/g, '$1 $2');
+						name = name.replace(/\s+/, ' ').trim();
+						fonts.push({ name, sources: [path.basename(file)] });
+					}
+				});
+			}
+
+			// font.name should be lowercase+underscore
+			fonts.forEach(font => {
+				font.sources = font.sources.filter(s => !s.startsWith('//'));
+				font.name = font.name.toLowerCase().replace(/\s/g, '_');
+				font.sizeIn = font.sources.reduce(
+					(sum, source) => sum + fs.statSync(resolve(dir, source)).size, 0
+				);
+				font.dirInFont = dir;
+				font.dirOutFont = resolve(outputDir, font.name)
+
+				todos.push(font)
+			});
+		}
+	});
+
+	return todos;
+}
+
+async function makeGlyphs(font) {
+	if (!fs.existsSync(font.dirOutFont)) fs.mkdirSync(font.dirOutFont);
+
+	const sourceFonts = {};
+	font.sources.forEach(sourceName => {
+		sourceFonts[sourceName] = fs.readFileSync(resolve(font.dirInFont, sourceName));
+	});
 
 	let sizeSum = 0;
-	// would be much faster in parallel, but this is better for logging
-	for (let font of fonts) {
-		process.stdout.write(`   Font [${font.name}]`);
-		let size = await makeGlyphs(font);
-		process.stdout.write(` - ${Math.round(sizeSum/1024)} KB\n`);
-		sizeSum += size;
+	for (let start = 0; start < 65536; start += 256) {
+		const end = start + 255;
+
+		let results = await Promise.all(font.sources.map(sourceName => {
+			let source = sourceFonts[sourceName];
+			if (!source) throw Error(`[${font.name}] Source "%{sourceName}" not found`);
+
+			return new Promise((resolve, reject) => {
+				fontnik.range(
+					{ font: source, start, end },
+					(err, data) => {
+						if (err) reject(); else resolve(data);
+					}
+				);
+			});
+		}))
+
+		results = results.filter(r => r);
+
+		let combined = glyphCompose.combine(results);
+		sizeSum += combined.length;
+
+		fs.writeFileSync(resolve(font.dirOutFont, `${start}-${end}.pbf`), combined);
 	}
-	return sizeSum;
 
-	async function makeGlyphs(config) {
-		const sourceFonts = {};
-		const folderName = outputDir + '/' + config.name;
-
-		config.sources.forEach(sourceName => {
-			try {
-				sourceFonts[sourceName] = fs.readFileSync(dir + '/' + sourceName) || null;
-			} catch (e) { }
-		});
-
-		if (!fs.existsSync(folderName)) fs.mkdirSync(folderName);
-
-		let ranges = [];
-		for (let i = 0; i < 65536; (i = i + 256)) {
-			ranges.push([i, Math.min(i + 255, 65535)]);
-		}
-
-		let sizeSum = 0;
-		const histogram = new Array(256);
-		if (DEBUG) {
-			for (let range of ranges) await processRange(range);
-		} else {
-			await Promise.all(ranges.map(range => processRange(range)));
-		}
-
-		return sizeSum;
-
-		async function processRange(range) {
-			const [start, end] = range;
-
-			let results = await Promise.all(config.sources.map(sourceName => {
-				let source = sourceFonts[sourceName];
-				if (!source) {
-					console.log('[%s] Source "%s" not found', config.name, sourceName);
-					return;
-				}
-
-				return new Promise((resolve, reject) => {
-					fontnik.range(
-						{ font: source, start, end },
-						(err, data) => {
-							if (err) reject(); else resolve(data);
-						}
-					);
-				});
-			}))
-
-			results = results.filter(r => !!r);
-
-			let combined = glyphCompose.combine(results);
-			let size = combined.length;
-			sizeSum += size;
-			histogram[start / 256] = size;
-			if (DEBUG) console.log('[%s] Range %s-%s size %s B', config.name, start, end, size);
-
-			fs.writeFileSync(folderName + '/' + start + '-' + end + '.pbf', combined);
-		}
-	}
-};
+	font.sizeOut = sizeSum;
+}
