6c4c09cc4eff2003a090f0d9619d99f0f25d8699
Michael Kreil <github@michael-kreil.de>
2024-04-09 08:47:23 +0200

    feat: detect non-tty terminals for progress, close #4

--- src/lib/progress.ts
+++ src/lib/progress.ts
@@ -4,12 +4,18 @@ export class Progress {
 	private value: number;
 	private timeStart: number;
 	private readonly maxValue: number;
+	private readonly isTTY: boolean;
+	private readonly stepCount: number;
+	private lastStep: number;
 
 	constructor(msg: string, maxValue: number) {
 		this.msg = msg;
 		this.maxValue = maxValue;
 		this.value = 0;
 		this.timeStart = Date.now();
+		this.isTTY = process.stderr.isTTY;
+		this.stepCount = this.isTTY ? 100 : 5;
+		this.lastStep = -100;
 		this.render();
 	}
 
@@ -26,16 +32,27 @@ export class Progress {
 	finish() {
 		this.value = this.maxValue;
 		this.render();
-		process.stdout.write('\n');
+		process.stderr.write('\n');
 	}
 
 	private render() {
-		let eta = '';
-		if (this.value > this.maxValue / 100) {
-			const timeLeft = (Date.now() - this.timeStart) * (this.maxValue - this.value) / this.value / 1000;
-			eta = `- ${Math.floor(timeLeft / 60)}:${(100 + Math.floor(timeLeft) % 60).toFixed(0).slice(1)}`;
-		}
-		const progress = (100 * this.value / this.maxValue).toFixed(1);
-		process.stdout.write(`\u001b[2K\r${this.msg}: ${progress}% ${eta}`);
+		const step = Math.floor(this.stepCount * this.value / this.maxValue);
+		if (step <= this.lastStep) return;
+		this.lastStep = step;
+
+		const progress = (100 * this.value / this.maxValue).toFixed(0);
+		process.stderr.write([
+			this.isTTY ? '\u001b[2K\r' : '',
+			this.msg, ': ',
+			progress, '%',
+			this.getETA(),
+			this.isTTY ? '' : '\n',
+		].join(''));
+	}
+
+	private getETA(): string {
+		if (this.value < this.maxValue / 100) return '';
+		const timeLeft = (Date.now() - this.timeStart) * (this.maxValue - this.value) / this.value / 1000;
+		return ` - ${Math.floor(timeLeft / 60)}:${(100 + Math.floor(timeLeft) % 60).toFixed(0).slice(1)}`;
 	}
 }
